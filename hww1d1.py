f18663db5ffca67200db3a08a00beee20faec43b
Hiyuhh <h.mubaidin00@gmail.com>
2024-03-25 15:25:07 -0500

    fixed task 4 and 5 errors

--- hww1d1.py
+++ hww1d1.py
@@ -7,7 +7,6 @@ if weather == "sunny":
 else:
     print("Take an umbrella!")
 
-
 # Task 2: Your Mood Today
 mood = "happy"
 if mood == "happy":
@@ -17,40 +16,38 @@ else:
 
 
 # 2. Python Naming Convention Practice
-    #fixed error in variable names as we use snake_case
-        #removed (<span class="hljs-number">x</span>) from code line >> is not python code 
-PI_VALUE = 3.14
+#fixed error in variable names as we use snake_case
+#removed (<span class="hljs-number">x</span>) from code line >> is not python code 
+PI_VALUE = 3.14 # in caps because pi is a constant value
 user_age = 25
 user_location = "New York"
 MAX_LIMIT = 1000
 
 
 #3. Python Data Types and type() Function
-    #identifying the data type of each variable
+#identifying the data type of each variable
 variable_a = "Hello, World!"
-# class string ()
 print (type(variable_a)) #confirm type
+
 variable_b = 23
-# class integer ()
 print(type(variable_b)) #confirm type
+
 variable_c = 3.14
-#class float ()
 print(type(variable_c)) #confirm type
+
 variable_d = True
-#class boolean ()
 print(type(variable_d)) #confirm type
 
 
 # 4. Arithmetic Operations in Daily Life
 # Task 1: Grocery Store Math
-amount = float(water_case + cat_food + redbull)
 water_case = 4.99
 cat_food = 35.99
 redbull = 12.99
+amount = float(water_case + cat_food + redbull) #changed (amount) location to fix error
 #calculated the total rounded amount to the nearest hundredth
 rounded_total = round(amount, 2)
 print("your total amount is", rounded_total) 
-
 
 #Task 2: Bank Interest
 savings = int(10000)
@@ -62,6 +59,7 @@ print("total amount saved", savings*interest+savings) #output should be 700 + 10
 # Task 1: Value Swapping
 a = 2
 b = 4
+a, b = b, a # fixed error in swapping values
 #swapping the two values
-print("a =" , b) # output should be 4
-print("b=", a) # output should be 2
+print(a) # output should be 4
+print(b) # output should be 2
